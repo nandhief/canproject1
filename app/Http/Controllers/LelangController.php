d4888aed0a0079a1ac67bc19bf7c7084e543e5f4
nandhief <nandhieffree@gmail.com>
2018-10-12 10:00:27 +0700

    Crud Lelang

---
+++ app/Http/Controllers/LelangController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lelang;
+use Illuminate\Http\Request;
+
+class LelangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lelang = Lelang::get();
+        if (request()->ajax()) {
+            foreach ($lelang as $key => $value) {
+                $data[] = collect($value)->prepend($key+1, 'no');
+            }
+            return response()->json(compact('data'), 200);
+        }
+        return view('lelang.index', compact('lelang'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('lelang.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'short_desc' => 'max:255',
+            'description' => 'required',
+        ]);
+        $lelang = Lelang::create($request->all());
+        return redirect()->route('lelang.show', $lelang->id)->withSuccess('Data berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Lelang  $lelang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lelang $lelang)
+    {
+        return view('lelang.show', compact('lelang'));
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Lelang  $lelang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lelang $lelang)
+    {
+        return view('lelang.edit', compact('lelang'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Lelang  $lelang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lelang $lelang)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'short_desc' => 'max:255',
+            'description' => 'required',
+        ]);
+        $lelang->update($request->all());
+        return redirect()->route('lelang.show', $lelang->id)->withSuccess('Data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Lelang  $lelang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lelang $lelang)
+    {
+        $lelang->delete();
+        return redirect()->route('lelang.index')->withSuccess('Data berhasil dihapus');
+    }
+}
---
+++ resources/views/lelang/show.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.lte')
+
+@section('title')
+	Detail Lelang
+@endsection
+
+@section('content-header')
+	<h1>
+		Lelang
+		<small>Detail Lelang {{ $lelang->name }}</small>
+	</h1>
+@endsection
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3>Detail Lelang {{ $lelang->name }}</h3>
+				</div>
+				<div class="box-body">
+					<div class="table-responsive">
+						<table class="table table-hover tabler-bordered">
+							<tr>
+								<th>NAMA</th>
+								<td>{{ $lelang->name }}</td>
+							</tr>
+							<tr>
+								<th>Terbit</th>
+								<td>
+									{!! $lelang->status ? '<span class="label label-success">Terbit</span>' : '<span class="label label-warning">Draf</span>' !!}
+								</td>
+							</tr>
+							<tr>
+								<th>Short Description</th>
+								<td>{{ $lelang->short_desc }}</td>
+							</tr>
+							<tr>
+								<th>Description</th>
+								<td>{!! $lelang->description !!}</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<a href="{{ route('lelang.index') }}" class="btn btn-flat btn-success"><i class="fa fa-reply"></i> Kembali</a>
+			<a href="{{ route('lelang.edit', $lelang->id) }}" class="btn btn-flat btn-warning"><i class="fa fa-edit"></i> Edit</a>
+			{{ Form::open(['route' => ['lelang.destroy', $lelang->id], 'method' => 'DELETE', 'style' => 'display: inline-block;']) }}
+				<button type="submit" class="btn btn-flat btn-danger" onclick="return confirm('Yakin Hapus Data')"><i class="fa fa-trash"></i> Hapus</button>
+			{{ Form::close() }}
+		</div>
+	</div>
+@endsection
