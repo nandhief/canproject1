22f4f4f492afb87762412540f19c883a803719d9
nandhief <nandhieffree@gmail.com>
2018-10-24 11:30:50 +0700

    CRUD Commodity

---
+++ app/Http/Controllers/CommodityController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commodity;
+use Illuminate\Http\Request;
+use App\Traits\Upload;
+
+class CommodityController extends Controller
+{
+    use Upload;
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $commodities = Commodity::get();
+        if (request()->ajax()) {
+            foreach ($commodities as $key => $value) {
+                $data[] = collect($value)->prepend($key+1, 'no');
+            }
+            return response()->json(compact('data'), 200);
+        }
+        return view('commodities.index', compact('commodities'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('commodities.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'short_desc' => 'max:255',
+            'description' => 'required',
+        ]);
+        $request = $this->saveFile($request);
+        $commoditi = Commodity::create($request->all());
+        return redirect()->route('commodities.show', $commoditi->id)->withSuccess('Data berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commodity $commodity)
+    {
+        return view('commodities.show', compact('commodity'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commodity $commodity)
+    {
+        return view('commodities.edit', compact('commodity'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commodity $commodity)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'short_desc' => 'max:255',
+            'description' => 'required',
+        ]);
+        $request = $this->saveFile($request);
+        $commodity->update($request->all());
+        return redirect()->route('commodities.show', $commodity->id)->withSuccess('Data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commodity $commodity)
+    {
+        $commodity->delete();
+        return redirect()->route('commodities.index')->withSuccess('Data berhasil dihapus');
+    }
+}
---
+++ resources/views/commodities/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.lte')
+
+@section('title')
+	Daftar Commodity
+@endsection
+
+@section('content-header')
+	<h1>
+		Commodity
+		<small>Daftar Commodity</small>
+	</h1>
+@endsection
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-primary">
+				<div class="box-header">
+					<a href="{{ route('commodities.create') }}" class="btn btn-sm btn-primary btn-flat"><i class="fa fa-plus"></i> Tambah</a>
+				</div>
+				<div class="box-body">
+					<div class="table-responsive">
+						<table class="table table-hover tabler-bordered {{ count($commodities) > 0 ? 'datatables':'' }}" style="width: 100%;">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>NAMA</th>
+									<th>KONTEN</th>
+									<th>PUBLISH</th>
+									<th data-orderable="false" data-searchable="false">&nbsp;</th>
+								</tr>
+							</thead>
+							<tbody>
+								@if (count($commodities) < 1)
+									<tr>
+										<td colspan="10">Tidak Ada Data</td>
+									</tr>
+								@endif
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('css_up')
+	<link rel="stylesheet" href="{{ asset('plugins') }}/datatables/dataTables.bootstrap.css">
+@endsection
+
+@section('js')
+	<script src="{{ asset('plugins') }}/datatables/jquery.dataTables.min.js"></script>
+	<script src="{{ asset('plugins') }}/datatables/dataTables.yajra.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('.datatables').dataTable({
+				ajax: "{{ route('commodities.index') }}",
+                "deferRender": true,
+                columns: [
+                	{ data: 'id' },
+                	{
+                		data: null,
+                		render: function (data) {
+                			return '<a href="{{ url('') }}/' + data.slug + '" target="_blank">' + data.name + '</a>';
+                		}
+                	},
+                	{ data: 'short_desc' },
+                	{
+                		data: null,
+                		render: function (data) {
+                			return data.status == 0 ? '<span class="label label-warning">Draf</span>' : '<span class="label label-info">Terbit</span>';
+                		}
+                	},
+                    {
+                        data: null,
+                        render: function (data) {
+                            return '<a href="{{ route('commodities.index') }}/' + data.id + '" class="btn btn-xs btn-flat btn-primary"><i class="fa fa-eye"></i> Lihat</a> <a href="{{ route('commodities.index') }}/' + data.id + '/edit" class="btn btn-xs btn-flat btn-info"><i class="fa fa-edit"></i> Edit</a> <form method="POST" action="{{  route('commodities.index') }}/' + data.id + '" accept-charset="UTF-8" style="display: inline-block;"><input name="_method" type="hidden" value="DELETE"><input name="_token" type="hidden" value="{{ csrf_token()  }}"> <button type="submit" class="btn btn-xs btn-flat btn-danger" onclick="return confirm(\'Anda Yakin Menghapus Data ' + data.name + '\')"><i class="fa fa-trash"></i> Hapus</button> </form>';
+                        }
+                    }
+                ]
+			});
+		});
+	</script>
+@endsection
