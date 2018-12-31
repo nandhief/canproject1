14a7d33840bbadc27cae4a22309014c4251744bc
nandhief <nandhieffree@gmail.com>
2018-12-31 10:51:01 +0700

    Get name to uppercase

--- app/Corporate.php
+++ app/Corporate.php
@@ -9,6 +9,12 @@ class Corporate extends Model
     protected $fillable = ['bagian', 'name', 'jabatan', 'path_foto'];
     protected $appends = ['foto'];
 
+
+    public function getNameAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
     public function getFotoAttribute()
     {
         return empty($this->path_foto) ? null : asset('storage/original/' . $this->path_foto);
